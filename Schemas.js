226b2109fce0641e526ea31a1b86d87eeecbbc3d
Igor Anzheyevskiy Zakharyk <igor_lifeking@hotmail.com>
2024-12-09 08:23:48 +0000

    feat: Added mongoose Schemas

---
+++ Schemas.js
@@ -0,0 +1,100 @@
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    }
+})
+
+userSchema.methods.toJson = function () {
+    return {
+        username: this.username,
+        _id: this.id
+    }
+}
+
+const exerciseSchema = new mongoose.Schema({
+    username: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+exerciseSchema.methods.toJson = function () {
+    return {
+        username: this.username.username,
+        description: this.description,
+        duration: this.duration,
+        date: this.date.toDateString(),
+        _id: this._id
+    }
+}
+
+exerciseSchema.methods.toJsonMin = function () {
+    return {
+        description: this.description,
+        duration: this.duration,
+        date: this.date.toDateString(),
+    }
+}
+
+const logSchema = new mongoose.Schema({
+    username: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    log: {
+        type: [Schema.Types.ObjectId],
+        ref: "Exercise"
+    }
+})
+
+logSchema.virtual("count").get(function () {
+    return this.log.length
+})
+logSchema.set('toJSON', { virtuals: true });
+logSchema.set('toObject', { virtuals: true });
+
+const populate = function (next) {
+    this.populate("username")
+    this.populate("log")
+    next()
+}
+
+exerciseSchema.pre("find", populate)
+exerciseSchema.pre("findOne", populate)
+logSchema.pre("find", populate)
+logSchema.pre("findOne", populate)
+
+logSchema.methods.toJson = function () {
+    return {
+        username: this.username.username,
+        count: this.count,
+        _id: this._id,
+        log: this.log.map(e => e.toJsonMin())
+    }
+}
+
+let User = mongoose.model("User", userSchema)
+let Exercise = moongose.model("Exercise", exerciseSchema)
+let Log = moongose.model("Log", logSchema)
+
+module.exports = {
+    User,
+    Exercise,
+    Log
+}
