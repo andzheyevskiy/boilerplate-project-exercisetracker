e0f58576be7dbc0f015531e34de26012db163281
Igor Anzheyevskiy Zakharyk <igor_lifeking@hotmail.com>
2024-12-09 08:28:53 +0000

    (installed mongoose)

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const mongoose = require("mongoose")
 
 app.use(cors())
 app.use(express.static('public'))
