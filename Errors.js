08196fa1089f6e690f01c2a0bbd2bd9695976891
Igor Anzheyevskiy Zakharyk <igor_lifeking@hotmail.com>
2024-12-10 08:28:03 +0000

    feat: error handling

---
+++ Errors.js
@@ -0,0 +1,31 @@
+class customError {
+    constructor(errorType, error = null) {
+        this.name = errorType.name
+        this.message = errorType.message
+        this.code = errorType.code
+        this.error = error
+    }
+}
+
+const DBErrors = {
+    Validation: {
+        name: "DB: Validation Error",
+        message: "Failed to validate item",
+        code: 500
+    },
+    Save: {
+        name: "DB: Save Error",
+        message: "Failed to save item",
+        code: 500
+    },
+    Find: {
+        name: "DB: Find Error",
+        message: "Failed to find item",
+        code: 500
+    }
+}
+
+module.exports = {
+    customError,
+    DBErrors
+}
--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 const mongoose = require("mongoose")
+const { Exercise, Log, User } = require('./Schemas')
+const { customError, DBErrors } = require('./Errors')
+
+mongoose.connect(process.env.MONGO_URI)
 
 app.use(cors())
 app.use(express.static('public'))
@@ -11,9 +15,29 @@ app.get('/', (req, res) => {
 });
 
 
+app.use(express.urlencoded({ extended: false }))
+
+app.post("/api/users", async function (req, res) {
+  postCreateUser(req, res)
+})
 
 
+
+// Error handling middleware
+app.use(function (err, req, res, next) {
+  res.status(err.code || 500).json({
+    error: {
+      code: err.code,
+      message: err.message
+    }
+  })
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
+
+
+// User Reset
+/* User.deleteMany({}).then(e=>console.log("Deleted: ",e))
+User.find({}).then(e=>console.log("Found", e))  */
